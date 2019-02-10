872c0df6ae8937d3bb8076b1905e68e6f200f739
yashdani99 <yashdani99@yahoo.com>
2019-02-09 21:47:12 -0800

    javascript for tutee

---
+++ tuteeSignUp.js
@@ -0,0 +1,20 @@
+function tuteeProfile(name, email, courses, topics, match) {
+  this.tuteeName = name;
+  this.tuteeEmail = email;
+  this.tuteeCourses = courses;
+  this.tuteeCourses = topics;
+  this.tuteeMatch = false;
+}
+
+function newTutee() {
+  var tuteeName = document.getElementById('tuteeName').value;
+  var tuteeEmail = document.getElementById('tuteeEmail').value;
+  var tuteeCourses = document.getElementById('tuteeCourses').value;
+  tuteeCourses = tuteeCourses.split(",");
+  var tuteeTopics = document.getElementById('tuteeTopics').value;
+  var tuteeMatch = false;
+
+  var person = new tuteeProfile(tuteeName, tuteeEmail, tuteeCourses, tuteeCourses, tuteeMatch);
+
+  allProfiles.splice(allProfiles.length, 0, person);
+}
